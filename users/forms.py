006b045ca20c602170956df99f72b96cc5fbfe96
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-12-23 12:25:33 +0545

    rename edit-profile to billing-address

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,8 @@
-from typing import Text
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+
+from users.models import UserInformation
 
 
 class UserForm(UserCreationForm):
@@ -16,6 +17,11 @@ class UserForm(UserCreationForm):
 
 
 class UserInformationForm(forms.Form):
-    phone = forms.CharField(max_length=10, required=True, label='Phone Number')
+    phone = forms.CharField(
+        max_length=10, required=True, label='Phone Number')
     delivery_address = forms.CharField(
         max_length=100, required=True, label='Delivery Address')
+
+    class Meta:
+        model = UserInformation
+        fields = ('phone', 'delivery_address')
--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('edit-profile/', views.editProfile, name='edit-profile'),
+    path('billing-address', views.billingAddress, name='billing-address'),
     path('profile/', views.profile, name='profile'),
 ]
