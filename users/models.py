a69551f52f0bbab30bd02374c903c41c9c1ad738
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-12-23 08:29:41 +0545

    change phone max_length

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # user personal information for renting
 class UserInformation(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
-    phone = models.CharField(max_length=100, blank=False, null=False)
+    phone = models.CharField(max_length=10, blank=False, null=False)
     delivery_address = models.CharField(
         max_length=100, blank=False, null=False)
 
