308dbae4a2ca4f244b85ff4e843b5cb36e2f1df4
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-12-31 20:08:48 +0545

    product model

--- app/models/product.py
+++ app/models/product.py
@@ -6,6 +6,20 @@ class Product(models.Model):
     description = models.TextField(null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
     image = models.ImageField(upload_to='product_images', null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
+
+
+class ProductInfo(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    stock = models.IntegerField(null=False, blank=False)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name
+
