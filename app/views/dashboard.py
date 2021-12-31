bd460008bbd0770ebe5d7f5f193d8352dc67448e
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-12-31 19:44:32 +0545

    dashboard base html

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -2,6 +2,11 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-# @login_required(login_url='/login/')
+@login_required(login_url='/login/')
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    return render(request, 'db_base.html')
+
+
+@login_required(login_url='/login/')
+def add_product(request):
+    return render(request, 'add_product.html')
