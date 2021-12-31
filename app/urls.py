531a91d60d4730a38050205a67880bf34d2bff95
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-12-31 19:43:56 +0545

    update url

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('dashboard/add-product/', views.add_product, name='add_product'),
 ]
