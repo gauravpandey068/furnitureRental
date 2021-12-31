ac3a8d850b0029f0b011f0931d881a801a6463c5
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-12-31 21:58:45 +0545

    add new product form

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from app.forms import ProductForm, ProductInfoForm
 
 
 @login_required(login_url='/login/')
@@ -9,4 +11,26 @@ def dashboard(request):
 
 @login_required(login_url='/login/')
 def add_product(request):
-    return render(request, 'add_product.html')
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)  # request.FILES is for image
+        if form.is_valid():
+            form.save()
+            return redirect('add_product_info', )
+
+    else:
+        form = ProductForm()
+
+    return render(request, 'add_product.html', {"form": form})
+
+
+@login_required(login_url='/login/')
+def add_product_info(request):
+    if request.method == 'POST':
+        form = ProductInfoForm(request.POST)
+        if form.is_valid():
+            print(form)
+            return redirect('dashboard')
+
+    else:
+        form = ProductInfoForm()
+    return render(request, 'add_product_info.html', {"form": form})
