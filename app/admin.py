17b95263e6e86847db6fe3f4b3e9db12ed8ce161
Gaurav Pandey <pandeygaurav068@gmail.com>
2021-12-31 20:09:08 +0545

    product model import in admin site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from app.models import Product, ProductInfo
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    list_filter = ('price', 'created_at', 'updated_at')
+    search_fields = ['name']
+
+
+class ProductInfoAdmin(admin.ModelAdmin):
+    list_display = ('product', 'stock', 'available')
+    list_filter = ('stock', 'available')
+    search_fields = ['product']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductInfo, ProductInfoAdmin)
