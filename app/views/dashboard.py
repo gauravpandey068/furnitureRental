2d91c7fc9eee9e1ffae84d68bd7d0063102d2bd3
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-01 10:30:17 +0545

    create view: add_product_info

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from app.forms import ProductForm, ProductInfoForm
+from app.models import Product
 
 
 @login_required(login_url='/login/')
@@ -14,8 +17,9 @@ def add_product(request):
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)  # request.FILES is for image
         if form.is_valid():
-            form.save()
-            return redirect('add_product_info', )
+            pk = form.save()
+            product_id = pk.id
+            return HttpResponseRedirect(f'{product_id}/add-product-info')
 
     else:
         form = ProductForm()
@@ -24,13 +28,14 @@ def add_product(request):
 
 
 @login_required(login_url='/login/')
-def add_product_info(request):
+def add_product_info(request, product_id):
+    product = Product.objects.get(id=product_id)
     if request.method == 'POST':
         form = ProductInfoForm(request.POST)
         if form.is_valid():
-            print(form)
+            form.save()
             return redirect('dashboard')
 
     else:
-        form = ProductInfoForm()
-    return render(request, 'add_product_info.html', {"form": form})
+        form = ProductInfoForm(initial={'product': product.id})
+    return render(request, 'add_product_info.html', {"form": form, "product": product}, )
