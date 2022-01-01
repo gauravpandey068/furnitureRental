e97e03c315053be3f23c3807ae174df5126f5635
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-01 10:29:18 +0545

    add product id while update product info

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/add-product/', views.add_product, name='add_product'),
-    path('dashboard/add-product/add-product-info/', views.add_product_info, name='add_product_info'),
+    path('dashboard/add-product/<product_id>/add-product-info/', views.add_product_info, name='add_product_info'),
 ]
