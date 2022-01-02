1e29361d2640bca9e868cedd31896ba00f92e69a
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-02 08:45:35 +0545

    optimise Product Model

--- app/models/product.py
+++ app/models/product.py
@@ -5,21 +5,11 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=False, blank=False)
     description = models.TextField(null=False, blank=False)
     price = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False)
-    image = models.ImageField(upload_to='product_images', null=False, blank=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.name
-
-
-class ProductInfo(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_image/%y/%m/%d', null=False, blank=False)
     stock = models.IntegerField(null=False, blank=False)
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.product.name
-
+        return self.name
