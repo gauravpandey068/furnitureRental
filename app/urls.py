2745f7ed237af022512db522c09b9242df7f8a0f
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-02 08:46:04 +0545

    media and static file path

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from . import views
 
@@ -6,5 +9,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/add-product/', views.add_product, name='add_product'),
-    path('dashboard/add-product/<product_id>/add-product-info/', views.add_product_info, name='add_product_info'),
 ]
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
