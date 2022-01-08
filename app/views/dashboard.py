87edd1293701de986ef715cb246ef1d26c5c174f
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-08 09:05:26 +0545

    edit product product details

--- app/views/dashboard.py
+++ app/views/dashboard.py
@@ -34,3 +34,27 @@ def delete_product(request, product_id):
     product = Product.objects.get(id=product_id)
     product.delete()
     return redirect('dashboard')
+
+
+@login_required(login_url='/login/')
+@staff_member_required()
+def edit_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)  # request.FILES is for image
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            description = form.cleaned_data['description']
+            price = form.cleaned_data['price']
+            image = form.cleaned_data['image']  # bug
+            brand = form.cleaned_data['brand']
+            available = form.cleaned_data['available']
+
+            Product.objects.filter(id=product_id).update(name=name, description=description, price=price, image=image,
+                                                         brand=brand, available=available)
+            return redirect('dashboard')
+
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, 'add_product.html', {"form": form})
