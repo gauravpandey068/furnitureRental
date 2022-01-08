a406dc81fc6c4edecc5f05fad2e78aa0f7f1c9d4
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-08 09:04:59 +0545

    product list and details page

--- app/urls.py
+++ app/urls.py
@@ -5,10 +5,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/add-product/', views.add_product, name='add_product'),
     path('dashboard/product/delete/<int:product_id>/', views.delete_product, name='delete_product'),
+    path('dashboard/product/edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('home/<int:product_id>/', views.products_detail, name='product_detail'),
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
