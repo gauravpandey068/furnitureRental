bf02d73c1e084d9f151d12f4c36fd4070c0ac8fd
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-01-11 09:29:36 +0545

    add created_at field in Rent

--- app/models/rent.py
+++ app/models/rent.py
@@ -21,6 +21,8 @@ class Rent(models.Model):
     is_returned = models.BooleanField(default=False)
     status = models.CharField(max_length=10, choices=Status.choices, default=Status.pending)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'{self.user} - {self.product}'
